0d9a2eb83e9d6eda10036d709f9409dd328f4a77
Someone <lucasviniciussoaresdasilva50@gmail.com>
2025-06-30 19:08:46 -0300

    Add files via upload

---
+++ Blox Fruits/ModulesBloxFruits.lua
@@ -0,0 +1,64 @@
+	FruitsId = {
+		["rbxassetid://15124425041"] = "Rocket Fruit",
+		["rbxassetid://15123685330"] = "Spin Fruit",
+		["rbxassetid://15123613404"] = "Blade Fruit",
+		["rbxassetid://15123689268"] = "Spring Fruit",
+		["rbxassetid://15123595806"] = "Bomb Fruit",
+		["rbxassetid://15123677932"] = "Smoke Fruit",
+		["rbxassetid://15124220207"] = "Spike Fruit",
+		["rbxassetid://121545956771325"] = "Flame Fruit",
+		["rbxassetid://15123673019"] = "Sand Fruit",
+		["rbxassetid://15123618591"] = "Dark Fruit",
+		["rbxassetid://77885466312115"] = "Eagle Fruit",
+		["rbxassetid://15112600534"] = "Diamond Fruit",
+		["rbxassetid://15123640714"] = "Light Fruit",
+		["rbxassetid://15123668008"] = "Rubber Fruit",
+		["rbxassetid://15123662036"] = "Ghost Fruit",
+		["rbxassetid://15123645682"] = "Magma Fruit",
+		["rbxassetid://15123606541"] = "Quake Fruit",
+		["rbxassetid://15123606541"] = "Buddha Fruit",
+		["rbxassetid://15123643097"] = "Love Fruit",
+		["rbxassetid://15123681598"] = "Spider Fruit",
+		["rbxassetid://116828771482820"] = "Creation Fruit",
+		["rbxassetid://15123679712"] = "Sound Fruit",
+		["rbxassetid://15123654553"] = "Phoenix Fruit",
+		["rbxassetid://15123656798"] = "Portal Fruit",
+		["rbxassetid://15123670514"] = "Rumble Fruit",
+		["rbxassetid://15123652069"] = "Pain Fruit",
+		["rbxassetid://15123587371"] = "Blizzard Fruit",
+		["rbxassetid://15123633312"] = "Gravity Fruit",
+		["rbxassetid://15123648309"] = "Mammoth Fruit",
+		["rbxassetid://15694681122"] = "T-Rex Fruit",
+		["rbxassetid://15123624401"] = "Dough Fruit",
+		["rbxassetid://15123675904"] = "Shadow Fruit",
+		["rbxassetid://10773719142"] = "Venom Fruit",
+		["rbxassetid://15123616275"] = "Control Fruit",
+		["rbxassetid://11911905519"] = "Spirit Fruit",
+		["rbxassetid://15123638064"] = "Leopard Fruit",
+		["rbxassetid://15487764876"] = "Kitsune Fruit",
+		["rbxassetid://115276580506154"] = "Yeti Fruit",
+		["rbxassetid://118054805452821"] = "Gas Fruit",
+		["rbxassetid://95749033139458"] = "Dragon Fruit"
+	}
+	
+	BerriesImage = {
+		["Blue Icicle Berry"] = "rbxassetid://86520396343538",
+		["Green Toad Berry"] = "rbxassetid://91948271748927",
+		["Orange Berry"] = "rbxassetid://74050626899602",
+		["Pink Pig Berry"] = "rbxassetid://101490686556249",
+		["Purple Jelly Berry"] = "rbxassetid://93939448612031",
+		["Red Cherry Berry"] = "rbxassetid://118278340780007",
+		["White Cloud Berry"] = "rbxassetid://79670000915648",
+		["Yellow Star Berry"] = "rbxassetid://93720145607080"
+	}
+		
+	ChestNames = {
+		["SilverChest"] = {"Silver Chest", "rgb(192, 192, 192)"},
+		["GoldChest"] = {"Gold Chest", "rgb(255, 215, 0)"},
+		["DiamondChest"] = {"Diamond Chest", "rgb(0, 255, 255)"}
+	}
+	
+	Flowers = {
+		["Flower1"] = {Name = "Blue Flower", Color = "rgb(0,0,170)"}, 
+		["Flower2"] = {Name = "Red Flower", Color = "rgb(170,0,0)"},
+	}
